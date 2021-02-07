9719a60d3127f59da07c0e3b6aa6f48434858cf9
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-02-07 12:13:40 +0600

    Add files via upload

---
+++ sum odd and even/sum odd and even/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int i, num1, num2, odd = 0, even=0;
+
+        // Reading number
+        Console.Write("Enter any number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        for (i = 1; i <= num1; i += 2)
+        {
+            //Adding current odd number to sum variable
+            odd += i;
+        }
+        Console.WriteLine("Sum of all odd numbers between 1 to " + num1 + " = " + odd);
+        Console.ReadLine();
+
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        for (i = 0; i <= num2; i += 2)
+        {
+            //Adding current odd number to sum variable
+            even += i;
+        }
+        Console.WriteLine("Sum of all odd numbers between 1 to " + num2 + " = " + even);
+
+        Console.ReadLine();
+    }
+}
