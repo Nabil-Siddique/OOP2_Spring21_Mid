77dc52d5ad7dca69af4f1badd6cf4786ba85ea93
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-02-07 12:19:21 +0600

    Add files via upload

--- sum odd and even/sum odd and even/Program.cs
+++ sum odd and even/sum odd and even/Program.cs
@@ -29,7 +29,7 @@ public class csharpExercise
             //Adding current odd number to sum variable
             even += i;
         }
-        Console.WriteLine("Sum of all odd numbers between 1 to " + num2 + " = " + even);
+        Console.WriteLine("Sum of all even numbers between 1 to " + num2 + " = " + even);
 
         Console.ReadLine();
     }
