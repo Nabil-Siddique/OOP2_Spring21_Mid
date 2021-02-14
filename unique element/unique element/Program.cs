99dfdeb7881f7d5db40aa36c8bdb3b04be0c95f9
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-02-14 13:47:26 +0600

    Add files via upload

---
+++ unique element/unique element/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public class Exercise6
+{
+    public static void Main()
+    {
+        int n, ctr = 0;
+        int[] arr1 = new int[5] { 1, 1, 2, 3, 4 };
+        int i, j, k;
+       
+        for (i = 0; i < 5; i++)
+        {
+            ctr = 0;
+           
+            for (j = 0; j < i - 1; j++)
+            {
+               
+                if (arr1[i] == arr1[j])
+                {
+                    ctr++;
+                }
+            }
+           
+            for (k = i + 1; k < 5; k++)
+            {
+               
+                if (arr1[i] == arr1[k])
+                {
+                    ctr++;
+                }
+               
+                if (arr1[i] == arr1[i + 1])
+                {
+                    i++;
+                }
+            }
+           
+            if (ctr == 0)
+            {
+                Console.Write("{0} ", arr1[i]);
+            }
+        }
+        Console.Write("\n\n");
+    }
+}
