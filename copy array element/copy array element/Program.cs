afbe925ea9b908a5dc53d914ac4ca1b3a94f71ee
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-02-14 13:23:25 +0600

    Add files via upload

---
+++ copy array element/copy array element/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Exercise4
+{
+    public static void Main()
+    {
+        int[] arr1 = new int[5] { 1, 2, 3, 4, 5 };
+        int[] arr2 = new int[5];
+        int i;
+
+        for (i = 0; i < 5; i++)
+        {
+            arr2[i] = arr1[i];
+        }
+
+       
+        Console.Write("\nThe elements stored in the first array are :\n");
+        for (i = 0; i < 5; i++)
+        {
+            Console.Write("{0}  ", arr1[i]);
+        }
+
+        
+        Console.Write("\n\nThe elements copied into the second array are :\n");
+        for (i = 0; i < 5; i++)
+        {
+            Console.Write("{0}  ", arr2[i]);
+        }
+        Console.Write("\n\n");
+    }
+}
