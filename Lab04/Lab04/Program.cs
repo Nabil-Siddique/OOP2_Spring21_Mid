042e1fe6bd4c7f1bb0ba4546c984c6812fc58f6c
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-02-28 20:08:03 +0600

    Add files via upload

---
+++ Lab04/Lab04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Survey Corp", "SC");
+            Department d2 = new Department("Military Police", "MP");
+            Student s1 = new Student("Eren", "1001", 3.2f);
+            Student s2 = new Student("Annie", "2060", 3.7f);
+            Student s3 = new Student("Mikasa", "1020", 4.0f);
+            s1.Department = d1;
+            s2.Department = d2;
+            s3.Department = d1;
+            s1.ShowInfo();
+            s2.ShowInfo();
+            s3.ShowInfo();
+        }
+    }
+}
---
+++ Lab04/Lab04/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Student
+    {
+
+        private string name;
+        private string id;
+        private float cgpa;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public float Cgpa
+        {
+
+            get { return cgpa; }
+            set { cgpa = value; }
+
+        }
+
+
+        Department department;
+
+        public Department Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public Student() { }
+
+        public Student(string name, string id, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("CGPA: " + cgpa);
+            department.ShowInfo();
+        }
+    }
+}
